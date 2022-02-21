c40babcd71f82ac0923a89f50db9ceb0e2f16afd
cjsrhd94 <cjsrhd94@gmail.com>
2022-02-22 00:36:25 +0900

    fix: Fix JPA metamodel must not be empty! error

---
+++ src/main/java/com/cjsrhd94/book/web/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.cjsrhd94.book.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
