17f54230b27831194484c62250a03064903bc968
cjsrhd94 <cjsrhd94@gmail.com>
2022-02-25 04:06:42 +0900

    feat: 구글 소셜 로그인 기능 구현

--- src/main/java/com/cjsrhd94/book/web/IndexController.java
+++ src/main/java/com/cjsrhd94/book/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.cjsrhd94.book.web;
 
 import com.cjsrhd94.book.service.posts.PostsService;
+import com.cjsrhd94.book.web.config.auth.dto.SessionUser;
 import com.cjsrhd94.book.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,15 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName1", user.getName());
+        }
+
         return "index";
     }
 
